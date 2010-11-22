4d34bd9ea2858dc668e560153bd53475a0f4593e
Arlindo Pereira <nighto@nighto.net>
2010-11-22 00:49:09 -0200

    Adding jQuery Mobile to the <head>.

--- header.php
+++ header.php
@@ -10,6 +10,10 @@
 	   <meta name="robots" content="noindex, nofollow" /> 
 	<?php } ?>
 
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.4.3.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.js"></script>
+
 	<title>
 		   <?php
 		      if (function_exists('is_tag') && is_tag()) {
